4cbf57198716f119a7bae295d007792ebda9a58b
Erik Garcia Tacas <erik.fisi16@gmail.com>
2023-10-10 22:47:24 -0500

    feat: add tailwind

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { IonicModule } from '@ionic/angular';
+import { RouterLinkActive, RouterLinkWithHref } from '@angular/router';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
+  imports: [CommonModule, IonicModule, RouterLinkWithHref, RouterLinkActive],
+})
+export class SharedModule {}
